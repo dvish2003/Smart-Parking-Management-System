bb9aae88eb5fb98869ec63fb26c83c88c818b6b9
vishan <Vishanchathuranga2003@gmail.com>
2025-06-14 02:45:08 +0530

    Smart Parking System

---
+++ CloudeConfig/src/main/java/lk/ijse/cloudeconfig/CloudeConfigApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.cloudeconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudeConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudeConfigApplication.class, args);
+	}
+
+}
